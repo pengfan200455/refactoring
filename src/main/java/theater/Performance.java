4bae26646f94b83a1d0ee2bb3a18cd43e966dab4
pengfan200455 <pengfan200455@gmail.com>
2025-11-23 14:21:57 -0500

    task 1

--- src/main/java/theater/Performance.java
+++ src/main/java/theater/Performance.java
@@ -1,15 +1,40 @@
 package theater;
 
 /**
- * Class representing a performance of a play..
+ * Class representing a performance of a play.
  */
 public class Performance {
 
-    public String playID;
-    public int audience;
+    private final String playID;
+    private final int audience;
 
+    /**
+     * Create a performance with the given play id and audience size.
+     *
+     * @param playID   the identifier of the play
+     * @param audience the audience size
+     */
     public Performance(String playID, int audience) {
         this.playID = playID;
         this.audience = audience;
     }
+
+    /**
+     * Return the play id of this performance.
+     *
+     * @return the play id
+     */
+    public String getPlayID() {
+        return playID;
+    }
+
+    /**
+     * Return the audience size of this performance.
+     *
+     * @return the audience size
+     */
+    public int getAudience() {
+        return audience;
+    }
 }
+
--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,39 @@
 package theater;
 
+/**
+ * Class representing information about a play.
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private final String name;
+    private final String type;
 
+    /**
+     * Create a play with the given name and type.
+     *
+     * @param name the display name of the play
+     * @param type the type of the play (for example, tragedy or comedy)
+     */
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    /**
+     * Return the name of this play.
+     *
+     * @return the play name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Return the type of this play.
+     *
+     * @return the play type
+     */
+    public String getType() {
+        return type;
     }
 }
